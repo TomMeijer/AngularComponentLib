030382275311f83475da22a1fb322575bc1bba66
Tom Meijer <tommeijer95@hotmail.com>
2020-08-03 22:58:40 +0200

    add storybook

--- projects/bs-lib/src/lib/bs-lib.module.ts
+++ projects/bs-lib/src/lib/bs-lib.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BsLibComponent } from './bs-lib.component';
 import { TableComponent } from './table/table.component';
 import {CommonModule} from "@angular/common";
+import {PaginationModule} from "ngx-bootstrap/pagination";
 
 @NgModule({
   declarations: [BsLibComponent, TableComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PaginationModule
   ],
   exports: [BsLibComponent]
 })
